a7186695d8b31b3a13fda6249ce01f15edc18828
Gaming-Priest <tekpriest.org@gmail.com>
2020-12-07 22:51:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,42 +4,35 @@ import QuoteCard from './quote-card'
 
 const App = () => {
   const [state, setState] = useState({
-    getQuote:[],
     quote: '',
     author: '',
   });
-  const END_POINT = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+  const END_POINT = 'https://api.quotable.io/random'
 
 
-  // const Data = async () => {
-  // const result = await fetch(END_POINT)
-  //     .then(response => response.json())
-  //     .then(data => data)
-  //     .catch(error => console.log(error));
-  // return result
-  // }
-  
-  const Data = async ()=>{
+  const getData = async ()=>{
     const results = await axios({
       method:'get',
       url:END_POINT
     })
-    .then(res=>res.data)
+    .then(res=>{
+      return res.data;
+    })
     .catch(error=>console.log(error)) 
     setState({
-      getQuote:results
+      quote:results.content,
+      author:results.author
     })
   }
-  useEffect({
-    Data()
+  useEffect(()=>{
+    getData()
   },[])
   
-  console.log(state);
   return (
     <Fragment> 
-      <QuoteCard /> 
+      <QuoteCard author={state.author} quote={state.quote} getData={getData}/> 
     </Fragment>
   );
-};
+};;
 
-export default App;
+  export default App; 
--- src/quote-card.js
+++ src/quote-card.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const QuoteCard = () => {
+const QuoteCard = ({quote,author,getData}) => {
     return ( 
         <div className="card-group">
           <div className="card">
             <div className="card-body quote-text">
               <i className="fa fa-quote-left" aria-hidden="true"></i>
-              <h1 className="card-text">ipsum dolor sit amet, consectetur adipisicing elit. Necessitati</h1>
+              <h4 className="card-text">{quote}</h4>
               <i className="fa fa-quote-right" aria-hidden="true"></i>
             </div>
             <div className="card-body quote-author">
-              <h5 className="card-text">author</h5>
+              <h6 className="card-text">{author}</h6>
             </div>
             <div className="quote-buttons">
               <a href="#"><i className="fab fa-twitter"></i></a>
-              <button className="btn btn-primary">New Quote</button>
+              <button className="btn btn-primary" onClick={getData}>New Quote</button>
             </div>
           </div>
         </div>
